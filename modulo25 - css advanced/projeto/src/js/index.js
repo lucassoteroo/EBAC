a8732f086ba4facace925f6122be56d4a25a0e42
lucassoteroo <lucassoteroo14@gmail.com>
2022-03-22 16:07:03 -0300

    melhorando estilos e adicionando interações

--- modulo25 - css advanced/projeto/src/js/index.js
+++ modulo25 - css advanced/projeto/src/js/index.js
@@ -1,21 +1,10 @@
 import '../scss/styles.scss'
-import imageBuilding from '../assets/building.jpg'
-import imageNotebook from '../assets/notebook.jpg'
 
-const elemRoot = document.getElementById('root')
-elemRoot.classList.add('container')
-
-const elemImageBuilding = new Image()
-elemImageBuilding.src = imageBuilding
-
-const elemImageNotebook = new Image()
-elemImageNotebook.src = imageNotebook
-
-/**
- document.querySelector('#building')
-     .appendChild(elemImageBuilding)
- 
- document.querySelector('#notebook')
-     .appendChild(elemImageNotebook)
- * 
- */
+document.addEventListener('click', (event) => {
+    const et = event.target;
+    if (et.classList.contains('vitrine__button')) {
+        alert('Sem produtos no momento, volte nos próximos módulos ;)')
+    } else if (et.classList.contains('contato__button')) {
+        alert('contato enviado com sucesso!')
+    }
+})
