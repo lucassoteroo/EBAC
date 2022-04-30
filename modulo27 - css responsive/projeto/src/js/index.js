9ae873fd8b1f250cd44659f5807a2ebdc95ef8dc
lucassoteroo <lucassoteroo14@gmail.com>
2022-04-30 12:07:29 -0300

    performando a validação do envio do formulário

--- modulo27 - css responsive/projeto/src/js/index.js
+++ modulo27 - css responsive/projeto/src/js/index.js
@@ -6,14 +6,44 @@ import '../assets/img/investir.jpg'
 import '../assets/img/planejar.jpg'
 import '../assets/img/vontade.jpg'
 
+let camposObrigatorios = document.querySelectorAll('input.contato__nome');
+let camposEmail = document.querySelectorAll('input.contato__email');
 
-document.addEventListener('click', (event) => {
-    const et = event.target;
-    let textoNome = document.getElementById('texto1').value;
-    let textoGmail = document.getElementById('texto2').value;
-    if (et.classList.contains('contato__button') && textoNome == '' && textoGmail == '') {
-        alert("Preencha os campos de 'Nome' e 'Email'")
-    } else if (et.classList.contains('contato__button')) {
-        alert(textoNome + " enviaremos nossa newsletter para o email: " + textoGmail)
-    }
-})
+const validaCampo = (elemento) => {
+    elemento.addEventListener('focusout', function(event) {
+        event.preventDefault();
+        if (this.value == "") {
+            alert("verifique se o campo 'nome' esta correto")
+            this.classList.add('erro')
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            this.classList.remove('erro');
+        }
+    });
+
+}
+
+const validaEmail = (elemento) => {
+    elemento.addEventListener('focusout', function(event) {
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+        if (this.value.match(emailValido)) {
+            event.preventDefault();
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+            document.getElementById('enviar').disabled = false
+        } else {
+            alert("verifique se o campo 'email' foi escrito corretamente")
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+for (let emFoco of camposObrigatorios) {
+    validaCampo(emFoco)
+}
+for (let emFoco of camposEmail) {
+    validaEmail(emFoco)
+}
