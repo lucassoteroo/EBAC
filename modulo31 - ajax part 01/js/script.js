cd83f4ddcabd7d04097afbb985278ee6be74cb1f
lucassoteroo <lucassoteroo14@gmail.com>
2022-05-23 11:31:28 -0300

    performando o envio do formulário para ativar o botão e limpar os inputs

--- modulo31 - ajax part 01/js/script.js
+++ modulo31 - ajax part 01/js/script.js
@@ -12,14 +12,12 @@ function getCep(user) {
         .then((res) => res.json())
         .then((data) => {
             let cep = data.cep
-            let button = document.querySelector('#botao')
             if (cep.length == 9) {
                 document.getElementById('idBairro').value = (data.bairro);
                 document.getElementById('idLocalidade').value = (data.localidade);
                 document.getElementById('idLogradouro').value = (data.logradouro);
                 document.getElementById('idDDD').value = (data.ddd);
                 document.getElementById('idUF').value = (data.uf)
-                button.disabled = false
             } else {
                 alert('Confira se o campo "CEP" está correto')
             }
@@ -27,18 +25,13 @@ function getCep(user) {
         .catch(() => alert('Confira se o campo "CEP" está correto'))
 }
 
-function Enviar() {
-    if (document.getElementById('idNome').value == ('') || document.getElementById('idNome').value == ('')) {
-        alert('Confira se os campos de "Nome" ou "Idade" estão preenchidos')
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    if (document.getElementById('idBairro').value == ('') || document.getElementById('idLocalidade').value == ('') || document.getElementById('idLogradouro').value == ('') || document.getElementById('idDDD').value == ('') || document.getElementById('idUF').value == ('') || document.getElementById('idNome').value == ('') || document.getElementById('idIdade').value == ('')) {
+        alert('Verifique se todos os campos estão preenchidos')
     } else {
-        alert('Formulário enviado com sucesso')
-        document.getElementById('idNome').value = ('')
-        document.getElementById('idIdade').value = ('')
-        document.getElementById('cepUsuario').value = ('')
-        document.getElementById('idBairro').value = ('');
-        document.getElementById('idLocalidade').value = ('');
-        document.getElementById('idLogradouro').value = ('');
-        document.getElementById('idDDD').value = ('');
-        document.getElementById('idUF').value = ('')
+        form.submit();
     }
-}
+})
