bf86059ce58f06219ee116a26a3ac0b34126abe6
lucassoteroo <lucassoteroo14@gmail.com>
2022-05-30 13:38:49 -0300

    performando a validação de cada input

--- modulo32 - ajax part 02/js/script.js
+++ modulo32 - ajax part 02/js/script.js
@@ -29,37 +29,70 @@ cepUsuario.addEventListener('focusout', function(event) {
 })
 
 function isEmpty(element) {
-    return element.value.length < 1 ? `O campo ${element.name} não pode estar vazio` : '';
+    if (element.value.length < 1) {
+        return `O campo ${element.name} não pode estar vazio`
+    } else {
+        return ''
+    }
 }
 
 function validaEmail(element) {
-    return element.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? '' : `Digite um ${element.name} válido`
+    if (element.value == element.value.match(/\S+@\S+\.\S+/)) {
+        return '';
+    } else {
+        return `Digite um ${element.name} válido`;
+    }
 }
 
 function validaNome(element) {
-    return element.value.match(/[A-Za-z]*/) ? '' : `Digite um ${element.name} válido`
+    if (element.value == element.value.match(/[A-Za-z]*/)) {
+        return '';
+    } else if (!isNaN(element.value)) {
+        return `O campo não aceita números`;
+    } else {
+        return `Digite um ${element.name} válida`;
+    }
 }
 
 function validaIdade(element) {
-    return element.value.match(/^\S[0-9]{0,3}$/) ? '' : `Digite uma ${element.name} válida`
+    if (element.value == element.value.match(/[0-9]{0,3}/)) {
+        return ''
+    } else {
+        return `Digite uma ${element.name} válida`
+    }
 }
 
 function validaUf(element) {
-    return element.value.match(/[A-Z]{2}/) ? '' : `Digite uma ${element.name} válida`
+    if (element.value == element.value.match(/[A-Za-z]{2}/)) {
+        return ''
+    } else {
+        return `Digite uma ${element.name} válida`
+    }
 }
 
 function validaDDD(element) {
-    return element.value.match(/[0-9]{2}/) ? '' : `Digite uma ${element.name} válida`
+    if (element.value == element.value.match(/[0-9]{2}/)) {
+        return '';
+    } else {
+        return `Digite uma ${element.name} válida`
+    }
 }
 
 function validaNumero(element) {
-    return element.value.match(/[0-9]{9}/) ? '' : `Digite uma ${element.name} válida`
+    if (element.value == element.value.match(/[0-9]{9}/)) {
+        return '';
+    } else {
+        return `Digite uma ${element.name} válida`
+    }
 }
 
 function validaCep(element) {
-    return element.value.match(/[0-9]{5}[\d]{3}/) ? '' : `Confira se o campo "${element.name}" está correto`
+    if (element.value == element.value.match(/[0-9]{5}[\d]{3}/)) {
+        return '';
+    } else {
+        return `Confira se o campo "${element.name}" está correto`;
+    }
 }
-
 
 function getCep(user) {
     fetch(`https://viacep.com.br/ws/${user}/json/`, {
@@ -77,11 +110,75 @@ function getCep(user) {
                 logradouro.value = (data.logradouro);
                 ddd.value = (data.ddd);
                 uf.value = (data.uf)
+                for (let i = 0; i < 5; i++) {
+                    teste.push('validado')
+                }
                 campoResultadoCep.innerHTML = ''
             }
         })
 }
 
+// Validação Email
+function validaEmailOnChange() {
+    let variavelEmail = email
+    validacaoCampo(validaEmail(variavelEmail), campoResultadoEmail, teste)
+}
+
+// Validação Nome
+function validaNomeOnChange() {
+    let variavelNome = nome
+    validacaoCampo(validaNome(variavelNome), campoResultadoNome, teste)
+}
+
+// Validação Idade
+function validaIdadeOnChange() {
+    let variavelIdade = idade
+    validacaoCampo(validaIdade(variavelIdade), campoResultadoIdade, teste)
+}
+
+// Validação UF
+function validaUfOnChange() {
+    let variavelUf = uf
+    validacaoCampo(validaUf(variavelUf), campoResultadoUf, teste)
+}
+
+// Validação DDD
+function validaDddOnChange() {
+    let variavelDdd = ddd
+    validacaoCampo(validaDDD(variavelDdd), campoResultadoDdd, teste)
+}
+
+// Validação Número
+function validaNumOnChange() {
+    let variavelNum = num
+    validacaoCampo(validaNumero(variavelNum), campoResultadoNumero, teste)
+}
+
+// Validação Cep
+function validaCepOnChange() {
+    let variavelCep = cepUsuario
+    validacaoCampo(validaCep(variavelCep), campoResultadoCep, teste)
+}
+
+// Validação Rua
+function validaRuaOnChange() {
+    let variavelRua = logradouro
+    validacaoCampo(isEmpty(variavelRua), campoResultadoRua, teste)
+}
+
+// Validação Bairro
+function validaBairroOnChange() {
+    let variavelBairro = bairro
+    validacaoCampo(isEmpty(variavelBairro), campoResultadoBairro, teste)
+}
+
+// Validação Cidade
+function validaCidadeOnChange() {
+    let variavelCidade = localidade
+    validacaoCampo(isEmpty(variavelCidade), campoResultadoLocalidade, teste)
+}
+
+const teste = []
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -94,78 +191,20 @@ form.addEventListener('submit', function(event) {
         }
     })
 
-    const teste = []
+    console.log(teste.length)
 
-    // Validação do email
-    const isEmailValid = validaEmail(email);
-    const isEmailEmpty = isEmpty(email);
-    validaocaoFinal(isEmailEmpty, isEmailValid, campoResultadoEmail, teste)
-
-    // Validação do nome
-    const isNomeValid = validaNome(nome)
-    const isNomeEmpty = isEmpty(nome)
-    validaocaoFinal(isNomeEmpty, isNomeValid, campoResultadoNome, teste)
-
-    // Validação do bairro
-    const isBairroValid = validaNome(bairro)
-    const isBairroEmpty = isEmpty(bairro)
-    validaocaoFinal(isBairroEmpty, isBairroValid, campoResultadoBairro, teste)
-
-    // Validação da localidade
-    const isLocalidadeValid = validaNome(localidade)
-    const isLocalidadeEmpty = isEmpty(localidade)
-    validaocaoFinal(isLocalidadeEmpty, isLocalidadeValid, campoResultadoLocalidade, teste)
-
-    // Validação da idade
-    const isIdadeValid = validaIdade(idade)
-    const isIdadeEmpty = isEmpty(idade)
-    validaocaoFinal(isIdadeEmpty, isIdadeValid, campoResultadoIdade, teste)
-
-    // Validação do UF
-    const isUfValid = validaUf(uf)
-    const isUfEmpty = isEmpty(uf)
-    validaocaoFinal(isUfEmpty, isUfValid, campoResultadoUf, teste)
-
-    // Validação do DDD
-    const isDddValid = validaDDD(ddd)
-    const isDddEmpty = isEmpty(ddd)
-    validaocaoFinal(isDddEmpty, isDddValid, campoResultadoDdd, teste)
-
-    // Validação do Numero
-    const isNumeroValid = validaNumero(num)
-    const isNumeroEmpty = isEmpty(num)
-    validaocaoFinal(isNumeroEmpty, isNumeroValid, campoResultadoNumero, teste)
-
-    // Validacao do cep
-    const isCepEmpty = isEmpty(cepUsuario)
-    const isCepValid = validaCep(cepUsuario)
-    validaocaoFinal(isCepEmpty, isCepValid, campoResultadoCep, teste)
-
-    // Validação do Rua
-    const isRuaValid = validaNome(logradouro)
-    const isRuaEmpty = isEmpty(logradouro)
-    validaocaoFinal(isRuaEmpty, isRuaValid, campoResultadoRua, teste)
-
-    if (campoResultadoNome.innerHTML && campoResultadoEmail.innerHTML && campoResultadoIdade.innerHTML && campoResultadoNumero.innerHTML && campoResultadoCep.innerHTML && campoResultadoRua.innerHTML && campoResultadoBairro.innerHTML && campoResultadoLocalidade.innerHTML && campoResultadoUf.innerHTML && campoResultadoDdd.innerHTML === '') {
-        console.log('oi')
-    } else {
-        console.log('deu ruim')
-    }
-
-    console.log(teste.length == 10)
-
-    if (teste.length == 10) {
+    if (teste.length > 10) {
         alert('Enviando formulário. Obrigado!')
         form.submit()
     }
 })
 
-const validaocaoFinal = function ValidacaoCampos(validacaoEmBranco, validaocaoCampo, campo, arr) {
-    if (validacaoEmBranco) {
-        campo.innerHTML = validacaoEmBranco
-    } else if (validaocaoCampo) {
-        campo.innerHTML = validaocaoCampo
+
+function validacaoCampo(validacao, campo, arr) {
+    if (validacao) {
+        campo.innerHTML = validacao
     } else {
         arr.push('validado')
+        campo.innerHTML = ''
     }
 }
